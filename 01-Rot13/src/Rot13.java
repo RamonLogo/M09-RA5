0160f4ad3d80c08a45b029f9abb93ce861fe8b5b
RamonLogo <ramonpl92@gmail.com>
2026-09-19 19:48:24 +0200

    xifrat extrany

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,43 +1,78 @@
 public class Rot13 {
-    
-    public static String xifraRot13 (String cadena) {
-        String resultat = "";
-        
-        for (int i = 0; i < cadena.length();i++) {
-            char c = cadena.charAt(i);
-
-            for (int j = 0; j < minuscules.length; j++) {
-                if (c == minuscules[i]) {
-                    c = (i + 13) % minuscules.length;
-                    resultat += c;
-                }
-            }         
-           for (int j = 0; j < majuscules.length; j++) {
-                if (c == majuscules[i]) {
-                    c = (i + 13) % majuscules.length;
-                    resultat += c;
-                }
-            } 
-            //resultat += c;  
-        }
-
-        return resultat;    
-    }
-
-    public static String desxifraRot13 (String cadena) {
-        String resultat = "";
-
-        return resultat;
-    }
-    
-    public static  final String ALFABET = "aàáäbçcdeèéëfghiíïjklmnñoòóöpqrstuùúüvwxyz"; 
-   
+    public static  final String ALFABET = "aàáäbçcdeèéëfghiíïjklmnñoòóöpqrstuùúüvwxyz";    
 
     public static final char[] minuscules = ALFABET.toCharArray();
     public static final char[] majuscules = ALFABET.toUpperCase().toCharArray();
 
-    public static void main(String[] args) {  
-       
+    public static String xifraRot13 (String cadena) {
+        String resultat = "";
+        int novaPos = 0;
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                
+                for (int j = 0; j < majuscules.length; j++) {
+                    
+                    if (c == majuscules[j]){
+                        novaPos = (j+13) % majuscules.length;
+                        c = majuscules[novaPos];                        
+                        resultat += c;
+                        break;
+                    }
+                }
+            } else if (Character.isLowerCase(c)) {
+                for (int k = 0; k < minuscules.length; k++) {
+                    if (c == minuscules[k]){
+                        novaPos = (k+13) % minuscules.length;
+                        c = minuscules[novaPos];               
+                        resultat += c;
+                        break;
+                    }
+                }
+            } else {
+                resultat += c;
+            }
+        }
+        return resultat;
+    }
+
+    public static String desxifraRot13 (String cadena) {
+        String resultat = "";
+        int novaPos = 0;
+        
+
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            if (Character.isUpperCase(c)) {
+                for (int j = 0; j < majuscules.length; j++) {
+                    if (c == majuscules[j]) {
+                        novaPos = (j - 13 + majuscules.length) % majuscules.length; 
+                        c = majuscules[novaPos];
+                        resultat += c;
+                        break;
+                    }
+                }
+            } else if (Character.isLowerCase(c)) {
+                for (int k = 0; k < minuscules.length; k++) {
+                    if (c == minuscules[k]) {
+                        novaPos = (k - 13 + minuscules.length) % minuscules.length; 
+                        c = minuscules[novaPos];
+                        resultat += c;
+                        break;
+                    }
+                }
+            } else {
+                resultat += c;
+            }
+
+        }
+        return resultat;
+    }
+
+    public static void main(String[] args) {
+        
         String msgs[] = {"ABC", "XYZ", "Hola, Mr.Calçot", "Perdó, per tu què és?"};
         String msgsXifrats[] = new String[msgs.length];
        
@@ -45,15 +80,14 @@ public class Rot13 {
 
         for (int i = 0; i<msgs.length; i++) {
             msgsXifrats[i] = xifraRot13(msgs[i]);
-            System.out.print("%-23s => %s%n", msgs[i], msgsXifrats[i]);
+            System.out.printf("%-23s => %s%n", msgs[i], msgsXifrats[i]);
         }
    
         System.out.println("\nDesxifrat\n------");
 
         for (String msg: msgsXifrats) {
-            System.out.print("%-23s => %s%n", msgs, DesxifratRot13(msg));
+            System.out.printf("%-23s => %s%n", msg, desxifraRot13(msg));
         }
-
+        
     }
-    
 }
